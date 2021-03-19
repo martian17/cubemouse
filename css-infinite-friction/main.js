0bf4ef66fe8307bd33a0c1bd3c82698258d6eafb
Yutaro Yoshii <yakumodanshi@gmail.com>
2021-03-20 00:23:28 +0900

    added the indinite friction version with momentum

--- css-infinite-friction/main.js
+++ css-infinite-friction/main.js
@@ -42,35 +42,55 @@ var resultMatrix = [
     0,0,0,1
 ];
 
-
+var vx = 0;
+var vy = 0;
 var x0 = 0;
 var y0 = 0;
+var t0 = 0;
 down = false;
 document.body.addEventListener("mousedown",function(e){
     down = true;
     x0 = e.clientX;
     y0 = e.clientY;
+    t0 = Date.now();
 });
 
 document.body.addEventListener("mousemove",function(e){
     if(!down)return false;
     var x = e.clientX;
     var y = e.clientY;
+    var t = Date.now();
+    var dt = (t - t0)/1000;
     var dx = x - x0;
     var dy = y - y0;
+    vx = dx/dt;
+    vy = dy/dt;
+    x0 = x;
+    y0 = y;
+    t0 = t;
+});
+
+document.body.addEventListener("mouseup",function(e){
+    down = false;
+});
+
+var start = 0;
+var animate = function(t){
+    if(start === 0)start = t;
+    var dt = (t - start)/1000;
+    start = t;
+    
+    var dx = dt*vx;
+    var dy = dt*vy;
     //remember small angle approximation? sin(x) x>0 := x
     var ay = -dx/100//destination angle for the y axis
     var ax = dy/100//destination angle for the x axis
     var ymat = genYmat(ay);
     var xmat = genXmat(ax);
     resultMatrix = matMul4(matMul4(resultMatrix,ymat),xmat);
-    x0 = x;
-    y0 = y;
-    
     //then finally plug in all the values
     cube.style.transform = "matrix3d("+resultMatrix.join(",")+")";
-});
+    requestAnimationFrame(animate);
+}
 
-document.body.addEventListener("mouseup",function(e){
-    down = false;
-});
+requestAnimationFrame(animate);
