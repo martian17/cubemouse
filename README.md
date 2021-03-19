e0cc7cb28a4b3d2a0b8682dafafff64bcf6abc4e
Yutaro Yoshii <yakumodanshi@gmail.com>
2021-03-20 01:31:41 +0900

    added the finite friction version

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 It's like a floating bearing ball but it's cubic --martian17
+
+Personally the infinite friction one is the most satisfying
--- css/main.js
+++ css/main.js
@@ -46,31 +46,43 @@ var vx = 0;
 var vy = 0;
 var x0 = 0;
 var y0 = 0;
-var t0 = 0;
-down = false;
+var t0 = Date.now();
+var down = false;
+
+var mvx = 0;//mouse velocity
+var mvy = 0;
+
+
 document.body.addEventListener("mousedown",function(e){
+    e.preventDefault();
     down = true;
     x0 = e.clientX;
     y0 = e.clientY;
     t0 = Date.now();
+    mvx = 0;
+    mvy = 0;
 });
 
 document.body.addEventListener("mousemove",function(e){
-    //if(!down)return false;
+    e.preventDefault();
+    if(!down)return false;
     var x = e.clientX;
     var y = e.clientY;
     var t = Date.now();
     var dt = (t - t0)/1000;
     var dx = x - x0;
     var dy = y - y0;
-    vx = dx/dt;
-    vy = dy/dt;
+    if(dt === 0)return false;//infinity->nan bug, preventing being frozen
+    mvx = dx/dt;
+    mvy = dy/dt;
+    console.log(dx,dy,mvx,mvy,dt);
     x0 = x;
     y0 = y;
     t0 = t;
 });
 
 document.body.addEventListener("mouseup",function(e){
+    e.preventDefault();
     down = false;
 });
 
@@ -79,15 +91,25 @@ var animate = function(t){
     if(start === 0)start = t;
     var dt = (t - start)/1000;
     start = t;
-    
+    if(down){
+        if(Date.now() - t0 > 100){
+            mvx = 0;
+            mvy = 0;
+            console.log("cancelled");
+        }
+        vx = vx+(mvx-vx)*dt*3;
+        vy = vy+(mvy-vy)*dt*3;//friction part
+    }
     var dx = dt*vx;
     var dy = dt*vy;
+    console.log(dx,dy);
     //remember small angle approximation? sin(x) x>0 := x
     var ay = -dx/100//destination angle for the y axis
     var ax = dy/100//destination angle for the x axis
     var ymat = genYmat(ay);
     var xmat = genXmat(ax);
     resultMatrix = matMul4(matMul4(resultMatrix,ymat),xmat);
+    console.log(resultMatrix);
     //then finally plug in all the values
     cube.style.transform = "matrix3d("+resultMatrix.join(",")+")";
     requestAnimationFrame(animate);
